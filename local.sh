3ccdb495c67c9d0acd6a0b7753da32cf870a72c0
ganesh <soccerganesh@gmail.com>
2020-09-15 02:22:17 +0530

    database setup

---
+++ local.sh
@@ -0,0 +1,5 @@
+export DB_NAME=e-cell-website
+export DB_USERNAME=postgres
+export DB_PORT=5432
+export DB_HOST=localhost
+# export DB_PASS=
--- website/settings.py
+++ website/settings.py
@@ -77,11 +77,31 @@ WSGI_APPLICATION = 'website.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': os.environ.get('DB_NAME'),
+
+        'USER': os.environ.get('DB_USERNAME'),
+
+        'PASSWORD': os.environ.get('DB_PASS'),
+
+        'HOST': os.environ.get('DB_HOST'),
+
+        'PORT': os.environ.get('DB_PORT'),
+
     }
+
 }
 
 
